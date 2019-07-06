f74134a6274533882330f151140fc2371f7f7d18
Hai Nguyen Tien <haitin4bk@gmail.com>
2019-07-07 01:04:42 +0700

    [Background] Formik Basic

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  SafeAreaView,
+  TextInput,
+  Button,
+  ActivityIndicator
+} from 'react-native';
+import { Formik } from 'formik';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={{ marginTop: 90 }}>
+      <Formik
+        initialValues={{ name: '' }}
+        onSubmit={(value, actions) => {
+          alert(JSON.stringify(value));
+          setTimeout(() => {
+            actions.setSubmitting(false);
+          }, 1000);
+        }}
+      >
+        {formikProps => (
+          <React.Fragment>
+            <TextInput
+              style={{
+                borderWidth: 1,
+                borderColor: 'black',
+                padding: 10,
+                marginBottom: 3
+              }}
+              onChangeText={formikProps.handleChange('name')}
+            />
+            {formikProps.isSubmitting ? (
+              <ActivityIndicator />
+            ) : (
+              <Button title="Submit" onPress={formikProps.handleSubmit} />
+            )}
+          </React.Fragment>
+        )}
+      </Formik>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
